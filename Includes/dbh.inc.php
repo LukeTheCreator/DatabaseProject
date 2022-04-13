30bedc9c2a64d6a8b341473419e1ed905e5f808a
Luke Norton <62163216+LukeTheCreator@users.noreply.github.com>
2022-04-12 22:09:31 -0400

    sql stuff
    added the sql file

--- Includes/dbh.inc.php
+++ Includes/dbh.inc.php
@@ -3,6 +3,11 @@
 $servername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
-$dbName = "DatabaseProject";
+$dbName = "databaseproject";
 
 $conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
+
+if(!$conn)
+{
+    die("Connection failed: " . mysqli_connect_error());
+}
